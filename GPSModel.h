d124d7eab0205475d9ffe808de5ec33316420dac
Natalie Josefsson <natalie.josefsson@ha.ax>
2015-04-29 13:40:33 +0300

    chaged to class and added constructor/destructor

--- GPSModel.h
+++ GPSModel.h
@@ -1,8 +1,29 @@
 #ifndef __GPSMODEL_H__
 #define __GPSMODEL_H__
 
-struct GPSModel
+class GPSModel
 {
+public:
+	GPSModel(
+		std::string timestamp,
+		double latitude,
+		double longitude,
+		double altitude,
+		double speed,
+		double heading,
+		int satellitesUsed
+	) :
+		timestamp(timestamp),
+		latitude(latitude),
+		longitude(longitude),
+		altitude(altitude),
+		speed(speed),
+		heading(heading),
+		satellitesUsed(satellitesUsed)
+	{};
+	
+	~GPSModel() {};
+
 	std::string timestamp;
 	double latitude;
 	double longitude;
