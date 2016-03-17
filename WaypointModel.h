5fc34f09d9b3d6bc266853b2b456ddea5e40e87f
Krister <krister1987@gmail.com>
2016-03-17 14:52:34 +0200

    Added file

--- WaypointModel.h
+++ WaypointModel.h
@@ -7,9 +7,10 @@
 class WaypointModel
 {
 public:	
-	WaypointModel(PositionModel positionModel, int radius, std::string id) :
+	WaypointModel(PositionModel positionModel, int radius, std::string id, int declination) :
 		positionModel(positionModel),
 		radius(radius),
+		declination(declination),
 		id(id),
 		time(0)
 	{};
@@ -18,6 +19,7 @@ public:
 
 	PositionModel positionModel;	
 	int radius;
+	int declination;
 	std::string id;
 	int time;
 };
